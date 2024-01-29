7ff1d7ce4230f9fea197a39732640bafca45ee3d
DmitryKostrov <dimon3limon3@gmail.com>
2024-01-29 22:23:50 +0300

    Bug_fixed
    Bug fixed: it was necessary to convert a string value to a numeric value.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
+    
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -15,7 +16,7 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+    let guess: u32 = guess.trim().parse().expect("!!!Please type a number!!!\n");
 
     println!("You guessed: {guess}");
     match guess.cmp(&secret_number) {
